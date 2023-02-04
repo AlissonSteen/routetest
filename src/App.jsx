4644844adab6730486824e69566f0ddc42bdbd7d
AlissonSteen <alisson.a.m88@gmail.com>
2023-02-03 21:00:01 -0300

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
         <Route path='agenda' element={<h2>Certo</h2>} />
         <Route path='calendar' element={<h1>Ok apareceu</h1>} />
         <Route path='teste' element={<h1>Oi</h1>} />
+        <Route path='teste' element={<h1>AAAAAA</h1>} />
       </Route>
     </Routes>
   )
